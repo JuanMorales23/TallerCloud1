efab6b7877afc515cb7141b9ffff605727fc57a6
Juan Morales <juanmr2311@gmail.com>
2021-11-26 19:16:36 -0500

    Configuracion inicial

---
+++ src/main/java/co/com/poli/tallercloud1/TallerCloud1Application.java
@@ -0,0 +1,13 @@
+package co.com.poli.tallercloud1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerCloud1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TallerCloud1Application.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/poli/tallercloud1/TallerCloud1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.poli.tallercloud1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerCloud1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
